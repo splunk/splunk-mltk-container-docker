d23d94b38f506609e035f1d610cc683d482906e4
Philipp Drieger <pdrieger@splunk.com>
2022-10-05 16:57:46 +0200

    refactor barebone to use new packaged support func

---
+++ app/model/barebone_template.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+    
+# In[1]:
+
+
+# this definition exposes all python module imports that should be available in all subsequent commands
+import json
+import numpy as np
+import pandas as pd
+# ...
+# global constants
+MODEL_DIRECTORY = "/srv/app/model/data/"
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+# In[6]:
+
+
+# this cell is not executed from MLTK and should only be used for staging data into the notebook environment
+def stage(name):
+    with open("data/"+name+".csv", 'r') as f:
+        df = pd.read_csv(f)
+    with open("data/"+name+".json", 'r') as f:
+        param = json.load(f)
+    return df, param
+
+
+
+
+
+
+
+
+
+
+
+    
+# In[10]:
+
+
+# initialize your model
+# available inputs: data and parameters
+# returns the model object which will be used as a reference to call fit, apply and summary subsequently
+def init(df,param):
+    model = {}
+    model['hyperparameter'] = 42.0
+    return model
+
+
+
+
+
+
+
+    
+# In[12]:
+
+
+# train your model
+# returns a fit info json object and may modify the model object
+def fit(model,df,param):
+    # model.fit()
+    info = {"message": "model trained"}
+    return info
+
+
+
+
+
+
+
+    
+# In[14]:
+
+
+# apply your model
+# returns the calculated results
+def apply(model,df,param):
+    y_hat = df.index
+    result = pd.DataFrame(y_hat, columns=['index'])
+    return result
+
+
+
+
+
+
+
+    
+# In[16]:
+
+
+# save model to name in expected convention "<algo_name>_<model_name>"
+def save(model,name):
+    with open(MODEL_DIRECTORY + name + ".json", 'w') as file:
+        json.dump(model, file)
+    return model
+
+
+
+
+
+    
+# In[17]:
+
+
+# load model from name in expected convention "<algo_name>_<model_name>"
+def load(name):
+    model = {}
+    with open(MODEL_DIRECTORY + name + ".json", 'r') as file:
+        model = json.load(file)
+    return model
+
+
+
+
+
+    
+# In[18]:
+
+
+# return a model summary
+def summary(model=None):
+    returns = {"version": {"numpy": np.__version__, "pandas": pd.__version__} }
+    return returns
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
