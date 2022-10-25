563deb31b56b57fe4a732b081d8d0087614f0f83
Philipp Drieger <pdrieger@splunk.com>
2022-10-25 18:50:52 +0200

    enforce important libraries override

--- bootstrap_spark.sh
+++ bootstrap_spark.sh
@@ -3,7 +3,7 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
 umask 002
-cp -R -n /dltk/app /srv
+cp -R /dltk/app /srv
 cp -R -n /dltk/notebooks /srv
 if [ -w /etc/passwd ]; then
   echo "dltk:x:$(id -u):0:dltk user:/dltk:/sbin/nologin" >> /etc/passwd
