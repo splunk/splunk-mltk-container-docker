d2da1cf1b5551f61c9f2ff1d815b6061f2e01142
jcowling <jcowling@splunk.com>
2023-06-15 15:34:19 +0100

    new pattern appears to work

--- dockerfiles-archive/bootstrap_root.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
-export FLASK_APP=/srv/app/index.py
-export FLASK_DEBUG=0
-
-jupyter lab --no-browser & tensorboard --bind_all --logdir /srv/notebooks/logs/ & flask run 
