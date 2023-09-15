784fa82140c033d3a1517cfd5f07d5425900cba2
Ubuntu <ubuntu@ip-172-31-19-137.eu-west-2.compute.internal>
2023-09-15 12:52:12 +0000

    rebuild support package without magic which caused error

--- package-dsdlsupport/src/dsdlsupport/SplunkSPLMagic.py
+++
@@ -1,8 +0,0 @@
-from IPython.core.magic import (Magics, magics_class, cell_magic)
-
-@magics_class
-class SplunkSPLMagic(Magics):
-
-    @cell_magic
-    def spl(self, line, cell):
-        return line, cell
--- package-dsdlsupport/src/dsdlsupport/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Init for SplunkSPLMagic"""
-__version__ = '1.0.0'
-
-import SplunkSPLMagic
-
-def load_ipython_extension(ipython):
-    ipython.register_magics(SplunkSPLMagic)
