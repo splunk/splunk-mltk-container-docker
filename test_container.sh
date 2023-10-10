3ce0aedc6ce37fc6a5b485c78af000c0cb9d2f33
Philipp Drieger <pdrieger@splunk.com>
2023-10-10 11:38:35 +0200

    add title to config test match

--- test_container.sh
+++ test_container.sh
@@ -17,15 +17,29 @@ else
   container_tag="$1"
 fi
 
+line=$(grep "^${container_tag}," tag_mapping.csv)
+
+base_image=$(echo $line | cut -d',' -f2)
+dockerfile=$(echo $line | cut -d',' -f3)
+base_requirements=$(echo $line | cut -d',' -f4)
+specific_requirements=$(echo $line | cut -d',' -f5)
+runtime=$(echo $line | cut -d',' -f6) 
+requirements_dockerfile=$(echo $line | cut -d',' -f7) 
+title=$(echo $line | cut -d',' -f8) 
+
+echo $line
+echo "Title:{$title}"
+
 files=$(find ./testing/test_mapping/ -type f | sort)
 found_match=false
 
 for file in $files; do
   echo $file
   echo $container_tag
+
   if grep -q "$container_tag" "$file"; then
     test_name=$(basename "$file")
-    pytest ./testing/test_$test_name.py -q --containername $container_tag -v
+    pytest ./testing/test_$test_name.py -q --containername "$title" -v
     found_match=true
   fi
 done
