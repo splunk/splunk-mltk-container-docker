c9aa3151cb512bf2805cbf6290972ffd1d65def6
Philipp Drieger <pdrieger@splunk.com>
2024-02-01 13:20:39 +0100

    fix issue #40

--- app/model/autoencoder.py
+++ app/model/autoencoder.py
@@ -3,7 +3,7 @@
 
 
     
-# In[ ]:
+# In[1]:
 
 
 # mltkc_import
@@ -13,7 +13,7 @@ import datetime
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from tensorflow import keras
+import keras
 
 # global constants
 MODEL_DIRECTORY = "/srv/app/model/data/"
@@ -25,7 +25,7 @@ MODEL_DIRECTORY = "/srv/app/model/data/"
 
 
     
-# In[ ]:
+# In[3]:
 
 
 # mltkc_stage
@@ -44,7 +44,7 @@ def stage(name):
 
 
     
-# In[ ]:
+# In[5]:
 
 
 # mltkc_init
@@ -81,7 +81,7 @@ def init(df,param):
 
 
     
-# In[ ]:
+# In[7]:
 
 
 # mltkc_stage_create_model_fit
@@ -121,7 +121,7 @@ def fit(model,df,param):
 
 
     
-# In[ ]:
+# In[9]:
 
 
 # mltkc_stage_create_model_apply
@@ -139,38 +139,40 @@ def apply(model,df,param):
 
 
 
-
-
     
-# In[ ]:
+# In[11]:
 
 
 # save model to name in expected convention "<algo_name>_<model_name>.h5"
 def save(model,name):
     # save keras model to hdf5 file
     # https://www.tensorflow.org/beta/tutorials/keras/save_and_restore_models
-    model.save(MODEL_DIRECTORY + name + ".h5")
+    model.save(MODEL_DIRECTORY + name + ".keras")
     return model
 
 
 
 
 
+
+
     
-# In[ ]:
+# In[13]:
 
 
 # load model from name in expected convention "<algo_name>_<model_name>.h5"
 def load(name):
-    model = keras.models.load_model(MODEL_DIRECTORY + name + ".h5")
+    model = keras.models.load_model(MODEL_DIRECTORY + name + ".keras")
     return model
 
 
 
 
 
+
+
     
-# In[ ]:
+# In[15]:
 
 
 # return model summary
@@ -182,12 +184,6 @@ def summary(model=None):
         model.summary(print_fn=lambda x: s.append(x+'\n'))
         returns["summary"] = ''.join(s)
     return returns
-
-
-
-
-
-
 
 
 
