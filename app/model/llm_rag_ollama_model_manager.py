8bdf577bab2b0bb4dad0731ad4cdb1d6bb4cf186
Huaibo Zhao <hzhao@splunk.com>
2024-06-20 14:09:07 +0900

    Enabled embedding model downloading

--- app/model/llm_rag_ollama_model_manager.py
+++ app/model/llm_rag_ollama_model_manager.py
@@ -11,6 +11,8 @@ import json
 import numpy as np
 import pandas as pd
 import requests
+import os
+from sentence_transformers import SentenceTransformer
 # ...
 # global constants
 ollama_url = "http://ollama:11434"
@@ -79,44 +81,67 @@ def fit(model,df,param):
 
 def apply(model,df,param):
     manager = param['options']['params']['task'].strip('\"')
-    if manager == "pull":
-        # Download specified model
-        try:
+    try:
+        model_type = param['options']['params']['model_type'].strip('\"')
+    except:
+        model_type = "LLM"
+    
+    if model_type == "embedder_model":
+        # embedder model
+        if manager == "pull":
+            model_name = param['options']['params']['model_name'].strip('\"').strip('\'')
+            modelPath = os.path.join("/srv/app/model/data", model_name)
+            try:
+                os.makedirs(modelPath)
+            except:
+                print("path already exists")
+            model = SentenceTransformer(model_name)
+            model.save(modelPath)
+            l = model_name
+            m = f"Downloaded embedder model {model_name}"
+        else:
+            print("Not supported")
+            l = "None"
+            m = "Not supported task"
+    else:
+        if manager == "pull":
+            # Download specified model
+            try:
+                model_name = param['options']['params']['model_name'].strip('\"')
+                uri = f"{ollama_url}/api/pull"
+                data = {
+                    "name": model_name
+                }
+                data = json.dumps(data)
+                requests.post(uri, data=data)
+                m = f'Pulled model {model_name}.'
+            except:
+                m = f'ERROR during model download.'
+            
+        elif manager == "delete":
+            # Delete specified model
             model_name = param['options']['params']['model_name'].strip('\"')
-            uri = f"{ollama_url}/api/pull"
+            uri = f"{ollama_url}/api/delete"
             data = {
                 "name": model_name
             }
             data = json.dumps(data)
-            requests.post(uri, data=data)
-            m = f'Pulled model {model_name}.'
-        except:
-            m = f'ERROR during model download.'
+            requests.delete(uri, data=data)
+            m = f'Deleted model {model_name}.'
+        else:
+            m = "No task specified"
         
-    elif manager == "delete":
-        # Delete specified model
-        model_name = param['options']['params']['model_name'].strip('\"')
-        uri = f"{ollama_url}/api/delete"
-        data = {
-            "name": model_name
-        }
-        data = json.dumps(data)
-        requests.delete(uri, data=data)
-        m = f'Deleted model {model_name}.'
-    else:
-        m = "No task specified"
-    
-    # List all existing models    
-    uri = f"{ollama_url}/api/tags"
-    response = requests.get(uri).json()
-    response = response['models']
-    try:
-        l = ""
-        for r in response:
-            l += r['model'].split(":")[0]
-            l += " "
-    except:
-        l = None
+        # List all existing models    
+        uri = f"{ollama_url}/api/tags"
+        response = requests.get(uri).json()
+        response = response['models']
+        try:
+            l = ""
+            for r in response:
+                l += r['model'].split(":")[0]
+                l += " "
+        except:
+            l = None
     l = [l]
     m = [m]
     cols={'Models': l, 'Message': m}
