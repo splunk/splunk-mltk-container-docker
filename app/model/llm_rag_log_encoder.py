cb989bf1af4b393c27198fd027594f17d7ed5e71
Huaibo Zhao <hzhao@splunk.com>
2024-06-28 10:47:34 +0900

    Added Compute function for log encoding

--- app/model/llm_rag_log_encoder.py
+++ app/model/llm_rag_log_encoder.py
@@ -245,7 +245,128 @@ def summary(model=None):
     returns = {"version": {"numpy": np.__version__, "pandas": pd.__version__} }
     return returns
 
+def compute(model,df,param):
+    model = {}   
+    pk_type=DataType.VARCHAR        
+    embedding_type=DataType.FLOAT_VECTOR
+    # Dimensionality setting of collection
+    try:
+        embedder_name = param['params']['embedder_name'].strip('\"')
+    except:
+        embedder_name = 'all-MiniLM-L6-v2'
+    # Dimension checking for default embedders
+    if embedder_name == 'intfloat/multilingual-e5-large':
+        n_dims = 1024
+    elif embedder_name == 'all-MiniLM-L6-v2':
+        n_dims = 384
+    else:
+        try:
+            n_dims=int(param['params']['embedder_dimension'])
+        except:
+            n_dims=384
+    
+    
+    # Collection name setting   
+    try:
+        collection_name=param['params']['collection_name'].strip('\"')
+    except:
+        collection_name="default_collection"
+    # Schema setting
+    try:
+        schema_fields=param['fieldnames']
+        schema_fields.remove(param['params']['label_field_name'])
+    except:
+        schema_fields=[]
 
+    print(schema_fields)
+        
+    print("start connecting to Milvus")
+    try:
+        # this hostname may need changing to a specific local docker network ip address depending on docker configuration
+        connections.connect("default", host="milvus-standalone", port="19530")
+        collection_exists = utility.has_collection(collection_name)
+        
+        # Basic schema setting
+        fields = [
+            FieldSchema(name="_key", is_primary=True, auto_id=True, dtype=DataType.INT64),
+            FieldSchema(name="embeddings", dtype=embedding_type, dim=n_dims),
+            FieldSchema(name="label", dtype=DataType.VARCHAR, max_length=15000),
+        ]
+        # Additional schema setting
+        if len(schema_fields) != 0: 
+            for i in range(len(schema_fields)):
+                fields.append(FieldSchema(name=schema_fields[i], dtype=DataType.VARCHAR, max_length=1000))
+        # Create schema
+        
+        schema = CollectionSchema(fields, f"dsdl schema for {collection_name}")
+        print(fields)
+        
+        if collection_exists:
+            print(f"The collection {collection_name} already exists")
+            collection = Collection(collection_name)
+            collection.load()
+        else:
+            print(f"The collection {collection_name} does not exist")
+            print(f"creating new collection: {collection_name}")
+            collection = Collection(collection_name, schema, consistency_level="Strong")
+            index = {
+                "index_type": "IVF_FLAT",
+                "metric_type": "L2",
+                "params": {"nlist": 1024},
+            }
+            collection.create_index("embeddings", index)
+    except:
+        collection = None
+    
+    model['collection']=collection
+    model['collection_name']=collection_name
+
+    if model['collection'] is not None:
+        use_local= int(param['params']['use_local'])
+        try:
+            embedder_name = param['params']['embedder_name'].strip('\"')
+        except:
+            embedder_name = 'all-MiniLM-L6-v2'
+        if use_local:
+            embedder_name = f'/srv/app/model/data/{embedder_name}'
+            print("Using local embedding model checkpoints")  
+        transformer_embedder = HuggingFaceEmbedding(model_name=embedder_name)
+
+        try:
+            label_field_name=param['params']['label_field_name']
+            print(label_field_name)
+            texts = []
+            vectors = []
+            for i in range(len(df)):
+                texts.append(df[i][label_field_name])
+                vectors.append(transformer_embedder.get_text_embedding(df[i][label_field_name]))
+            data=[vectors, texts]
+        except:
+            data = None
+            m = {"Message": "Failed. Could not vectorize dataframe. Check your field name."}
+            print(m)
+            
+        if data is not None:
+            if len(schema_fields) != 0:
+                for field in schema_fields:  
+                    l = []
+                    for i in range(len(df)):
+                        l.append(df[i][field])
+                    data.append(l)
+            try:
+                model['collection'].insert(data, timeout=None)
+                m = {"Message": "Success"}
+                print(m)
+            except:
+                m = {"Message": "Failed. Too much data to insert at once."}
+                print(m)
+    else:
+        m = {"Message": "Failed. Could not create collection. Check collection naming."}
+        print(m)
+    cols =[]
+    for _ in range(len(df)):
+        cols.append(m)
+    return cols
 
 
 
