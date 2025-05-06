1e9723509744c05455d268e88dabea333a5a9ae0
Philipp <pdrieger@splunk.com>
2025-05-06 14:04:03 +0200

    add number of workers option in bootstrap

--- bootstrap_scripts/bootstrap_fast.sh
+++ bootstrap_scripts/bootstrap_fast.sh
@@ -19,9 +19,19 @@ else
 fi
 
 if [ "$MODE_DEV_PROD" = "PROD" ]; then
-  echo "Starting in mode = PROD"
-  uvicorn app.main:app --host 0.0.0.0 --port 5000 $uvicorn_https_param
+  if [ -n "$api_workers" ]; then
+    echo "Starting in mode = PROD with $api_workers api workers"
+    uvicorn app.main:app --host 0.0.0.0 --workers $api_workers --port 5000 $uvicorn_https_param
+  else
+    echo "Starting in mode = PROD with 1 api workers"
+    uvicorn app.main:app --host 0.0.0.0 --workers 1 --port 5000 $uvicorn_https_param
+  fi
 else
-  echo "Starting in mode = DEV"
-  jupyter lab --no-browser --ip=0.0.0.0 --port=8888 & tensorboard --bind_all --logdir /srv/notebooks/logs/ & mlflow ui -p 6000 -h 0.0.0.0 & uvicorn app.main:app --host 0.0.0.0 --port 5000 --reload-dir app $uvicorn_https_param
+  if [ -n "$api_workers" ]; then
+    echo "Starting in mode = DEV with $api_workers api workers"
+    jupyter lab --no-browser --ip=0.0.0.0 --port=8888 & tensorboard --bind_all --logdir /srv/notebooks/logs/ & mlflow ui -p 6000 -h 0.0.0.0 & uvicorn app.main:app --host 0.0.0.0 --workers $api_workers --port 5000 --reload-dir app $uvicorn_https_param
+  else
+    echo "Starting in mode = DEV with default 1 api workers"
+    jupyter lab --no-browser --ip=0.0.0.0 --port=8888 & tensorboard --bind_all --logdir /srv/notebooks/logs/ & mlflow ui -p 6000 -h 0.0.0.0 & uvicorn app.main:app --host 0.0.0.0 --workers 1 --port 5000 --reload-dir app $uvicorn_https_param
+  fi
 fi
